7bccc72b7b88a8110d8568780052ac5a7689f34b
Guilherme Ferreira <alefff.guilherme@gmail.com>
2022-11-10 11:43:34 -0300

    Its

--- cypress/integration/helpers.spec.js
+++ cypress/integration/helpers.spec.js
@@ -23,4 +23,16 @@ describe('Helpers...', () => {
             console.log('Encontrei o segundo botão')
         })
     })
+    it.only('Its...', () => {
+        const obj = {nome: 'User', idade:20}
+        cy.wrap(obj).should('have.property', 'nome', 'User')
+        cy.wrap(obj).its('nome').should('be.equal', 'User')
+
+        const obj2 = {nome:'User', idadade:20, endereco: {rua: 'dos bobos'}}
+        cy.wrap(obj2).its('endereco').should('have.property', 'rua')
+        cy.wrap(obj2).its('endereco').its('rua').should('contain', 'dos bobos')
+        cy.wrap(obj2).its('endereco.rua').should('contain', 'dos bobos')
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.title().its('length').should('be.equal', 20)
+    })
 })
