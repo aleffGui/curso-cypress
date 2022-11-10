c2bde25643268ad3a185f782e3ff1d23314a33a4
Guilherme Ferreira <alefff.guilherme@gmail.com>
2022-11-10 14:08:57 -0300

    uso do find

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -23,4 +23,16 @@ describe('Sync...', () => {
             .should('exist')
             .type('funciona')
     })
+    it.only('Uso do find..', () => {
+        cy.get('#buttonList').click()
+        cy.get('#lista li')
+            .find('span')
+            .should('contain', 'Item 1')
+/* 
+        cy.get('#lista li')
+            .find('span')
+            .should('contain', 'Item 2')    */ 
+        cy.get('#lista li span')
+            .should('contain', 'Item 2')
+    })
 })
