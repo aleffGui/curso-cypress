f6344ac373039bb667f4d94082c1e5753e454c08
Guilherme Ferreira <alefff.guilherme@gmail.com>
2022-11-10 10:50:00 -0300

    checkbox

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -49,4 +49,13 @@ describe('Work with basic elements', () => {
             
         cy.get("[name='formSexo']").should('have.length', 2)    
     })
+    it.only('Checkbox', () => {
+        cy.get('#formComidaPizza')
+            .click()
+            .should('be.checked')
+
+        cy.get('[name=formComidaFavorita]').click({multiple: true})     
+        cy.get('#formComidaPizza').should('not.be.checked')
+        cy.get('#formComidaVegetariana').should('be.checked')    
+    })
 })
