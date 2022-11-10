7b76e038ed29b0823f42564fb0e5a720790dcf03
Guilherme Ferreira <alefff.guilherme@gmail.com>
2022-11-10 13:53:05 -0300

    retentativas

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -15,4 +15,12 @@ describe('Sync...', () => {
         cy.get('#novoCampo').should('exist')
         cy.get('#novoCampo').type('funciona')
     })
+    it('Deve fazer retrys', () => {
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#novoCampo')
+            .should('exist')
+            .type('funciona')
+    })
 })
