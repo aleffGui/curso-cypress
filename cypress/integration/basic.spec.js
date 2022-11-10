72ddb18016f910fb1bf91f38b124a9fd1f259c93
Guilherme Ferreira <alefff.guilherme@gmail.com>
2022-11-10 13:16:30 -0300

    reutilizando titulo

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -13,6 +13,20 @@ describe('Cypress Basic', () => {
         cy.title()
             .should('be.equal', 'Campo de Treinamento')
             .and('contain', 'Campo')
+        
+        let syncTitle;
+
+        cy.title().then(title => {
+            console.log(title)
+            cy.get("#formNome").type(title)
+            syncTitle = title
+        })
+        cy.get('[data-cy=dataSobrenome]').then(title => {
+            title.val(syncTitle)
+        })
+        cy.get('#elementosForm\\:sugestoes').then($el => {
+            cy.wrap($el).type(syncTitle)
+        })
     })
     it('Should find and interact with an element', () => {
         cy.visit('https://wcaquino.me/cypress/componentes.html');
